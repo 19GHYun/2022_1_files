7acbb2a9b39e30d418e73425e97c720bc0300b35
19GHYun <94778099+19GHYun@users.noreply.github.com>
2022-04-26 22:28:44 +0900

    Update 4장실습.cpp

--- 객체지향/eclipse-workspace/20194546/4장실습.cpp
+++ 객체지향/eclipse-workspace/20194546/4장실습.cpp
@@ -211,3 +211,46 @@ int main() {
 	}
 }
 */
+
+/*
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Circle {
+	int radius;
+public:
+	void setRadius(int radius);
+	double getArea();
+};
+void Circle::setRadius(int radius) {
+	this->radius = radius;
+}
+
+
+double Circle::getArea() {
+	return this->radius* this->radius * 3.14;
+}
+
+
+int main() {
+	Circle a[3];
+	int b = 0;
+	int count = 0;
+
+	for (int i = 0; i < 3; i++) {
+		cout << "원 " << i + 1 << "의 반지름 >> ";
+		cin >> b;
+		a[i].setRadius(b);
+
+	}
+
+	for (int i = 0; i < 3; i++) {
+		if (a[i].getArea() > 100) {
+			count++;
+		}
+	}
+
+	cout << "면적이 100보다 큰 원은 " << count << "개 입니다" << endl;
+}
+*/
