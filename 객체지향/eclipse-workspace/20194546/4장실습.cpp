9c046d7167f47d6e6ba110dbe7ca0ac8ebc5da88
19GHYun <94778099+19GHYun@users.noreply.github.com>
2022-04-27 14:25:40 +0900

    Update 4장실습.cpp

--- 객체지향/eclipse-workspace/20194546/4장실습.cpp
+++ 객체지향/eclipse-workspace/20194546/4장실습.cpp
@@ -254,3 +254,54 @@ int main() {
 	cout << "면적이 100보다 큰 원은 " << count << "개 입니다" << endl;
 }
 */
+
+/*
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+class Person{
+	string name;
+	string tel;
+public:
+	Person();
+	string getName() { return name;}
+	string getTel() {return tel;}
+	void set(string name, string tel);
+};
+Person::Person(){
+	name = "Blank";
+	tel = "010-1234-5678";
+}
+
+
+void Person::set(string name, string tel){
+	this->name = name;
+	this->tel = tel;
+}
+
+int main(){
+	cout << "이름과 전화 번호를 입력해 주세요" <<endl;
+
+	string a, b;
+
+	Person p[3];
+
+	for(int i = 0; i < 3 ; i++){
+	cout << "사람 " << i+1 << ">> ";
+	cin >> a >> b;
+	p[i].set(a,b);
+	}
+	cout<<"모든 사람의 이름은 " <<  p[0].getName() << " " << p[1].getName() << " " << p[2].getName() << endl;
+
+	cout << "전화번호 검색합니다. 이름을 입력하세요>>";
+
+	cin >> a;
+
+	for(int i = 0; i <3; i++){
+		if(p[i].getName() == a){
+			cout << "전화 번호는 " << p[i].getTel();
+		}
+	}
+}
+*/
